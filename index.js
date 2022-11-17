7c6098bafc99d13b8568c3b0ad539876b43906d5
Galiusbro <galiusbro@gmail.com>
2022-11-17 16:47:08 +0800

    query

--- index.js
+++ index.js
@@ -1,15 +1,21 @@
 const TelegramBot = require('node-telegram-bot-api');
+const express = require('express');
+const cors = require('cors')
 
 const token = '5708629352:AAFQ0t8Gk06FbpiJAUFNeSsUuWFVo_l8bIg';
 const webAppUrl = 'https://glittery-llama-98871a.netlify.app'
 
 const bot = new TelegramBot(token, {polling: true});
+const app = express();
+
+app.use(express.json());
+app.use(cors())
 
 bot.on('message', async (msg) => {
     const chatId = msg.chat.id;
     const text = msg.text;
 
-    if(text === '/start') {
+    if (text === '/start') {
         await bot.sendMessage(chatId, 'Ниже появится кнопка Заполни форму', {
             reply_markup: {
                 keyboard: [
@@ -27,7 +33,7 @@ bot.on('message', async (msg) => {
         });
     }
 
-    if(msg?.web_app_data?.data) {
+    if (msg?.web_app_data?.data) {
         try {
             const data = JSON.parse(msg?.web_app_data?.data)
 
@@ -43,3 +49,28 @@ bot.on('message', async (msg) => {
         }
     }
 });
+
+app.post('/web-data', async (req, res) => {
+    const {queryId, products, totalPrice} = req.body;
+
+    try {
+        await bot.answerWebAppQuery(queryId, {
+            type: 'article',
+            id: queryId,
+            title: 'Успешная покупка',
+            input_message_content: {message_text: 'Вы приобрели товар на сумму: ' + totalPrice}
+        })
+        return res.status(200).json({})
+    } catch (e) {
+        await bot.answerWebAppQuery(queryId, {
+            type: 'article',
+            id: queryId,
+            title: 'Не удалось приобрести товар',
+            input_message_content: {message_text: 'Не удалось приобрести товар'}
+        })
+    }
+    return res.status(500).json({});
+})
+
+const PORT = 8000;
+app.listen(PORT, () => console.log('Server started on PORT: ' + PORT))
